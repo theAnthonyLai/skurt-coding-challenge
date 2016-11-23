689a742516ba8d640e9f184c53461c72ac74e9a4
Anthony Lai <yukuan.anthony.lai@gmail.com>
2016-11-23 14:55:22 -0800

    Cleanup and prettify output

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import json
 import smtplib
 import urllib.request
-import time
+import datetime
 from threading import Timer
-
 from shapely.geometry import shape, Point, mapping
 
 SKURT_API_URL = 'http://skurt-interview-api.herokuapp.com/carStatus/'
 MAX_CAR_ID = 10
 MIN_CAR_ID = 1
+MONITOR_INTERVAL = 5 # (seconds) TODO
 
 class sendEmail:
     def __init__(self, email, password, recipient):
@@ -16,8 +16,7 @@ class sendEmail:
         self.password = password
         self.recipient = recipient
 
-    def send(self, subject, body):
-        
+    def send(self, subject, body, carId):
         msg = """From: %s\nTo: %s\nSubject: %s\n\n%s
         """ % (self.email, self.recipient, subject, body)
 
@@ -30,15 +29,14 @@ class sendEmail:
             server_ssl.login(self.email, self.password)
             server_ssl.sendmail(self.email, self.recipient, msg)
             server_ssl.close()
-            print("Email sent successfully!")
+            print('Alert email for car %d sent successfully!' % carId)
         except Exception as e:
             print(e)
 
 
 def checkCarStatus(carId, emailSender, emailFormat):
     print('Checking car %d' % carId)
-    #testVar = SKURT_API_URL + str(carId)
-    #print(testVar)
+
     try:
         res = urllib.request.urlopen(SKURT_API_URL + str(carId))
         text = res.read().decode('utf-8')
@@ -51,23 +49,24 @@ def checkCarStatus(carId, emailSender, emailFormat):
         if boundary.intersects(carLoc):
             print('Car %d within boundary' % carId)
         else:
-            print('Car %d out of boundary' % carId)
+            print('Car %d out of boundary. Sending alert email.' % carId)
+            emailSender.send(emailFormat['subject'], emailFormat['body'], carId)
 
     except Exception as e:
-            print(e)
+            print('Err getting car status:', e)
 
 
 def checkCarThread(carId, emailSender, emailFormat):
     # check current car
+    print('================================')
+    print('EVENT:', datetime.datetime.now())
     checkCarStatus(carId, emailSender, emailFormat)
 
     carId += 1
     if carId > MAX_CAR_ID:
         carId = MIN_CAR_ID
 
-    Timer(5.0, checkCarThread, (carId, emailSender, emailFormat)).start()
-    print('EVENT:', time.time())
-    #emailSender.send(emailFormat['subject'], emailFormat['body'])
+    Timer(MONITOR_INTERVAL, checkCarThread, (carId, emailSender, emailFormat)).start()
 
 
 def main():
@@ -84,30 +83,6 @@ def main():
 
 
     checkCarThread(MIN_CAR_ID, emailSender, emailFormat)
-
-    # try:
-    #     f = urllib.request.urlopen(SKURT_API_URL + '10')
-    #     text = f.read().decode('utf-8')
-    #     #print(text)
-    #     #print(type(text))
-
-    #     obj = json.loads(text)
-    #     #print(type(obj))
-    #     #print(obj)
-    #     #print(obj['features'][1]['geometry'])
-
-    #     boundary = shape(obj['features'][1]['geometry'])
-    #     #print(json.dumps(mapping(boundary)))
-    #     carLoc = Point(obj['features'][0]['geometry']['coordinates'][0], obj['features'][0]['geometry']['coordinates'][1])
-
-    #     # Returns True if the boundary and interior of the object intersect in any way with those of the other.
-    #     if boundary.intersects(carLoc):
-    #         print('Car within boundary')
-    #     else:
-    #         print('Car out of boundary')
-
-    # except Exception as e:
-    #         print(e)
 
 if __name__ == "__main__":
     main()
